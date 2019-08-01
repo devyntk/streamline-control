085d42f5bf2d0f1161347ce0496c5b71b0ccf179
bkeeneykid <bkeeneykid@me.com>
2019-07-31 19:55:13 -0500

    Config file handling

--- streamline/__main__.py
+++ streamline/__main__.py
@@ -1,7 +1,27 @@
-from PyQt5.QtWidgets import QApplication
-from streamline.MainWindow import MainWindow
+from PyQt5.QtWidgets import QApplication, QErrorMessage
 import sys
+import json
 
 app = QApplication(sys.argv)
-main = MainWindow()
-sys.exit(app.exec_())
+try:
+    local_config = open('config.json')
+except FileNotFoundError:
+    config = {
+        "type": "local",
+        "remote": {
+            "url": "http://example.com",
+            "auth": {
+                "user": "username",
+                "pass": "password"
+            }
+        },
+        "local_file": "~/Downloads/Your_File",
+        "encryption_key": None
+    }
+    f = open('config.json', 'w+')
+    f.write(json.dumps(config))
+    f.close()
+    msg = QErrorMessage()
+    msg.showMessage('No config file found. Please edit "config.json" in the "streamline-control" folder to configure.')
+
+app.exec_()
