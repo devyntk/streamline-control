1b1fa25115de14e4ab6a76b43e6e01f77942827b
Travis Weir <tweirtx@gmail.com>
2019-10-11 13:27:28 -0500

    New approach

---
+++ ci.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+if [ '$TRAVIS_OSNAME' == 'macos' ];
+then
+  curl pyenv.run | bash;
+else
+  cd /opt/pyenv/plugins/python-build/../.. && git pull origin master && git pull && cd -);
+fi
+pyenv install 3.6.8
+pyenv global 3.6.8
