ec4b670a7fad479b8bc82cc78588e349fb057611
Travis Weir <tweirtx@gmail.com>
2019-10-11 16:11:21 -0500

    cairo

--- ci.sh
+++ ci.sh
@@ -3,3 +3,7 @@ brew update && brew upgrade pyenv || echo "Not macOS"
 (cd /opt/pyenv/plugins/python-build/../.. && git pull origin master && cd -) || echo "Not Ubuntu"
 pyenv install 3.6.8
 pyenv global 3.6.8
+pip install pycairo
+pip install -Ur requirements.txt
+pip install pyinstaller
+pyinstaller streamline/__main__.py -F
