c6182a8add8e8675be53fdccc1303b16f2eeeb50
Travis Weir <tweirtx@gmail.com>
2019-10-12 17:05:23 -0500

    Fix Mac

--- ci.sh
+++ ci.sh
@@ -3,8 +3,8 @@ brew update && brew upgrade pyenv || echo "Not macOS"
 (cd /opt/pyenv/plugins/python-build/../.. && git pull origin master && cd -) || echo "Not Ubuntu"
 pyenv install 3.6.8
 pyenv global 3.6.8
-pip install pycairo
-pip install -Ur requirements.txt
-pip install pyinstaller
+pip3 install pycairo
+pip3 install -Ur requirements.txt
+pip3 install pyinstaller
 pyinstaller streamline/__main__.py -F
 mv dist/* "dist/streamline-control-$TRAVIS_OS_NAME"
