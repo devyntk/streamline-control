37c2ed52d44f9e491f56acc914779d71c39de9be
Travis Weir <tweirtx@gmail.com>
2019-10-12 17:17:50 -0500

    MacOS images are severely outdated

--- ci.sh
+++ ci.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-brew update && brew upgrade pyenv || echo "Not macOS"
+brew upgrade pyenv || echo "Not macOS"
 (cd /opt/pyenv/plugins/python-build/../.. && git pull origin master && cd -) || echo "Not Ubuntu"
 pyenv install 3.6.8
 pyenv global 3.6.8
