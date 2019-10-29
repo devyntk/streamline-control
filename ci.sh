58ad13dd7902f976e279a653a738dd55a4bdf5f3
Travis Weir <tweirtx@gmail.com>
2019-10-29 14:25:06 -0500

    Change where path is set

--- ci.sh
+++ ci.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 brew upgrade pyenv || echo "Not macOS"
-(cd /opt/pyenv/plugins/python-build/../.. && git pull origin master && cd - && export PATH="/opt/pyenv/bin:$PATH") || echo "Not Ubuntu"
+(cd /opt/pyenv/plugins/python-build/../.. && git pull origin master && cd -) || echo "Not Ubuntu"
+export PATH="/opt/pyenv/bin:$PATH"
 echo $PATH
 pyenv install 3.6.8
 pyenv global 3.6.8
