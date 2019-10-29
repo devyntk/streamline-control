f28e22c014a4dde860922d921f812332c3d8e6b0
Travis Weir <tweirtx@gmail.com>
2019-10-29 14:15:03 -0500

    Pyenv is not on path

--- ci.sh
+++ ci.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 brew upgrade pyenv || echo "Not macOS"
-(cd /opt/pyenv/plugins/python-build/../.. && git pull origin master && cd -) || echo "Not Ubuntu" && find / | grep pyenv
+(cd /opt/pyenv/plugins/python-build/../.. && git pull origin master && cd - && export PATH="/opt/pyenv:$PATH") || echo "Not Ubuntu"
 pyenv install 3.6.8
 pyenv global 3.6.8
 pip3 install pycairo
