894483b937f60029b30f9fd963285607650759f0
Travis Weir <tweirtx@gmail.com>
2019-10-29 14:01:16 -0500

    Windows is bad

--- ci.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-choco install python3
-export PATH="/c/Python37:/c/Python37/Scripts:$PATH"
-pip3 install pycairo
-pip3 install -Ur requirements.txt
-pip3 install pyinstaller
-pyinstaller streamline/__main__.py -F
-mv dist/* "dist/streamline-control-windows.exe"
