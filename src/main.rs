fa9ec78a5f2d36a0b960e174803e04d029de6f69
bkeeneykid <bkeeneykid@me.com>
2020-06-09 15:25:39 -0500

    GTK demo app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+extern crate gio;
+extern crate gtk;
+
+use gio::prelude::*;
+use gtk::prelude::*;
+
+use std::env::args;
+
+fn build_ui(application: &gtk::Application) {
+    let window = gtk::ApplicationWindow::new(application);
+
+    window.set_title("First GTK+ Program");
+    window.set_border_width(10);
+    window.set_position(gtk::WindowPosition::Center);
+    window.set_default_size(350, 70);
+
+    let button = gtk::Button::new_with_label("Click me!");
+
+    window.add(&button);
+
+    window.show_all();
+}
+
 fn main() {
-    println!("Hello, world!");
+    let application =
+        gtk::Application::new(Some("com.github.gtk-rs.examples.basic"), Default::default())
+            .expect("Initialization failed...");
+
+    application.connect_activate(|app| {
+        build_ui(app);
+    });
+
+    application.run(&args().collect::<Vec<_>>());
 }
