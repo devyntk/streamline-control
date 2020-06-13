409801586a1069f4980f7b6aae620a2964d46677
Travis Weir <tweirtx@gmail.com>
2020-06-13 13:58:34 -0500

    Start using another DMG creator

--- mackage.sh
+++ mackage.sh
@@ -48,5 +48,7 @@ mv "$MACOS_APP_DIR" "$MACOS_APP_NAME"
 ln -s /Applications "$MACOS_APP_NAME/Applications"
 rm -rf "$MACOS_APP_NAME/.Trashes"
 
-hdiutil create "uploads/$MACOS_APP_NAME.dmg" -srcfolder "$MACOS_APP_NAME" -ov
+pip install dmgbuild
+dmgbuild -s dmgset.py "Streamline Control" streamline.dmg
+#hdiutil create "uploads/$MACOS_APP_NAME.dmg" -srcfolder "$MACOS_APP_NAME" -ov
 rm -rf "$MACOS_APP_NAME"
