1cb44f91dd6cb64ea046da19cc033e64c14c4326
bkeeneykid <bkeeneykid@me.com>
2020-07-26 17:32:09 -0500

    Implement frontend page system

--- frontend/src/lib.rs
+++ frontend/src/lib.rs
@@ -4,40 +4,105 @@
 #![allow(clippy::wildcard_imports)]
 
 use seed::{prelude::*, *};
+mod page;
+
+const LOGIN: &str = "login";
+const DASH: &str = "dash";
 
 // ------ ------
 //     Init
 // ------ ------
 
 // `init` describes what should happen when your app started.
-fn init(_: Url, _: &mut impl Orders<Msg>) -> Model {
-    Model::default()
+fn init(url: Url, orders: &mut impl Orders<Msg>) -> Model {
+
+    let base_url = url.to_base_url();
+    orders
+        .subscribe(Msg::UrlChanged)
+        .notify(subs::UrlChanged(url.clone()));
+
+    Model {
+        ctx: Context {},
+        base_url,
+        page_id: Some(PageId::init(url, orders)),
+    }
 }
 
 // ------ ------
 //     Model
 // ------ ------
 
-// `Model` describes our app state.
-type Model = i32;
+struct Model {
+    ctx: Context,
+    base_url: Url,
+    page_id: Option<PageId>,
+}
+
+// ------ Context ------
+
+pub struct Context {
+}
+
+// ------ PageId ------
+
+#[derive(Copy, Clone, Eq, PartialEq)]
+enum PageId {
+    Login,
+    Dash,
+    NotFound
+}
+
+impl PageId {
+    fn init(mut url: Url, _orders: &mut impl Orders<Msg>) -> Self {
+        match url.next_path_part() {
+            None => Self::Login,
+            Some(LOGIN) => Self::Login,
+            Some(_) => Self::NotFound,
+        }
+    }
+}
 
 // ------ ------
 //    Update
 // ------ ------
 
-// (Remove the line below once any of your `Msg` variants doesn't implement `Copy`.)
-#[derive(Copy, Clone)]
 // `Msg` describes the different events you can modify state with.
 enum Msg {
-    Increment,
+    UrlChanged(subs::UrlChanged),
 }
 
 // `update` describes how to handle each `Msg`.
 fn update(msg: Msg, model: &mut Model, _: &mut impl Orders<Msg>) {
     match msg {
-        Msg::Increment => *model += 1,
+        Msg::UrlChanged(subs::UrlChanged(mut url)) => {
+            model.page_id = match url.next_path_part() {
+                None => Some(PageId::Login),
+                Some(LOGIN) => {
+                    Some(PageId::Login)
+                },
+                Some(DASH) => {
+                    Some(PageId::Dash)
+                }
+                Some(_) => None,
+            };
+        }
     }
 }
+
+// ------ ------
+//     Urls
+// ------ ------
+
+struct_urls!();
+impl<'a> Urls<'a> {
+    pub fn home(self) -> Url {
+        self.base_url()
+    }
+    // pub fn dash_urls(self) -> page::admin::Urls<'a> {
+    //     page::admin::Urls::new(self.base_url().add_path_part(ADMIN))
+    // }
+}
+
 
 // ------ ------
 //     View
@@ -46,11 +111,14 @@ fn update(msg: Msg, model: &mut Model, _: &mut impl Orders<Msg>) {
 // (Remove the line below once your `Model` become more complex.)
 #[allow(clippy::trivially_copy_pass_by_ref)]
 // `view` describes what to display.
-fn view(model: &Model) -> Node<Msg> {
-    div![
-        "This is a counter: ",
-        C!["counter"],
-        button![model, ev(Ev::Click, |_| Msg::Increment),],
+fn view(model: &Model) -> Vec<Node<Msg>> {
+    vec![
+        match model.page_id {
+            Some(PageId::Login) => div!["Login"],
+            Some(PageId::Dash) => div!["Dash"],
+            Some(PageId::NotFound) => div!["404"],
+            None => div!["404"],
+        },
     ]
 }
 
