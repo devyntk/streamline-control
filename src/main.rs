a473b48b482c8895aab162b62f7d4c32ff59bdda
Travis Weir <tweirtx@gmail.com>
2020-08-13 16:28:44 -0500

    First attempt at zeroconf registration

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 // For the `cargo_crate_version!` macro
 #[macro_use]
 extern crate self_update;
-
+use astro_dnssd;
 mod gui;
 mod update;
 
 fn main() {
+    let mut dns = astro_dnssd::register::DNSServiceBuilder::new("streamline");
+    dns = dns.with_port(80);
+    dns = dns.with_name("control");
+    dns.build().expect("Error creating DNS entry!");
     gui::run_ui();
 }
