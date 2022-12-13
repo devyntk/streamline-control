231149bc587115f570262796940c29539d4c2e96
devyntk <bkeeneykid@me.com>
2022-12-13 16:28:33 -0600

    functioning hiding sidebar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,8 +4,42 @@ import Sidebar from "./components/Sidebar.vue";
 </script>
 
 <template>
-  <Sidebar />
-  <RouterView />
+  <div class="app">
+    <Sidebar />
+    <div class="content-background">
+      <h2>{{ $route.name }}</h2>
+      <div class="content-wrapper"><RouterView /></div>
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+@import "./styles/colors";
+.app {
+  display: flex;
+  flex-wrap: nowrap;
+  align-items: stretch;
+}
+
+.content-background {
+  display: flex;
+  flex-wrap: nowrap;
+  flex-direction: column;
+  box-sizing: border-box;
+  padding: 1rem;
+  flex-grow: 1;
+  background-color: $accent;
+  h2 {
+    text-transform: capitalize;
+    font-size: 1.5rem;
+  }
+}
+.content-wrapper {
+  margin-top: 0.5rem;
+  box-sizing: border-box;
+  border-radius: 1rem;
+  width: 100%;
+  height: 100%;
+  background-color: white;
+}
+</style>
--- frontend/src/components/Sidebar.vue
+++ frontend/src/components/Sidebar.vue
@@ -2,28 +2,104 @@
 import { RouterLink } from "vue-router";
 import { ref } from "vue";
 
+import { ArrowRightIcon, HomeIcon } from "@heroicons/vue/24/outline";
+
 const open = ref(false);
 </script>
 
 <template>
   <nav class="sidebar" :class="{ open: open }">
-    <RouterLink to="/">Home</RouterLink>
-    <RouterLink to="/about">About</RouterLink>
-    <button @click="open = !open">{{ open ? "Close" : "Open" }}</button>
+    <div class="links">
+      <h1 class="title">Streamline Control</h1>
+      <RouterLink to="/">
+        <HomeIcon />
+        <div>Home</div>
+      </RouterLink>
+      <RouterLink to="/about"
+        ><HomeIcon />
+        <div>About</div></RouterLink
+      >
+    </div>
+    <div class="actions">
+      <div class="arrow" @click="open = !open">
+        <ArrowRightIcon />
+      </div>
+    </div>
   </nav>
 </template>
 
 <style scoped lang="scss">
 @import "../styles/colors";
 
+$closed-width: 3rem;
+$icon-margin: 0.75rem;
+
 .sidebar {
-  width: 3rem;
+  width: $closed-width;
   height: 100vh;
-  background: #8c8c8c;
-  transition: width 0.125s;
+  background: $background-accent;
+  transition: width 0.125s, margin 0.125s, height 0.125s;
+  display: flex;
+  flex-direction: column;
+  color: white;
+
+  .title {
+    width: 0;
+    height: 0;
+    margin: 0;
+    overflow: hidden;
+    white-space: nowrap;
+    font-size: 1.25rem;
+  }
+  a {
+    height: 2.5rem;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    svg {
+      box-sizing: border-box;
+      padding: $icon-margin;
+      width: $closed-width;
+    }
+    div {
+      width: 0;
+    }
+  }
+
+  .actions {
+    margin-top: auto;
+    display: flex;
+    flex-direction: row-reverse;
+    border-style: solid none;
+    border-width: 0.05rem;
+    .arrow {
+      svg {
+        transition: transform 0.125s;
+        transform: rotate(0deg);
+        padding: 0.5rem;
+      }
+      border-style: none solid;
+      border-width: 0.05rem;
+      display: flex;
+      width: $closed-width;
+      height: $closed-width;
+      cursor: pointer;
+    }
+  }
 
   &.open {
     width: 12rem;
+    .actions .arrow svg {
+      transform: rotate(180deg);
+    }
+    .title {
+      width: unset;
+      margin: $icon-margin;
+      height: unset;
+    }
+    a > div {
+      width: unset;
+    }
   }
 }
 </style>
